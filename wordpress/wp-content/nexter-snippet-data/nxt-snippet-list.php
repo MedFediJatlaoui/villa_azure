d822e3df542ff3677a017053b83fb265bf738b7f
Mahdi Ben Amor <mahdi.benamor@medianet.com.tn>
2026-06-27 20:52:26 +0100

    add the client fixes

--- wordpress/wp-content/nexter-snippet-data/nxt-snippet-list.php
+++ wordpress/wp-content/nexter-snippet-data/nxt-snippet-list.php
@@ -130,7 +130,7 @@ return array (
   'meta' => 
   array (
     'secret_key' => 'f1f06e3ae1e229c9b2b03f69bd2e797d',
-    'now_date' => '2026-06-27 11:00:11',
+    'now_date' => '2026-06-27 17:31:15',
     'version' => '4.6.12',
     'domain' => 'http://localhost:8080',
   ),
